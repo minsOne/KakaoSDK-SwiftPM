1c5b4f71ca75c51119243f93eacce3f312316278
JungMin Ahn <jungminahn.minsone@gmail.com>
2020-08-31 00:29:55 +0900

    SwiftPM 추가시 DynamicCodable를 선택목록에 안보이도록 수정

--- Package.swift
+++ Package.swift
@@ -11,9 +11,6 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DynamicCodable",
-            targets: ["DynamicCodable"]),
-        .library(
             name: "KakaoSDKCommon",
             targets: ["KakaoSDKCommon"]),
         .library(
