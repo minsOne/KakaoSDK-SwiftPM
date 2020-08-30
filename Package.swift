da74f6d3e2b6d3c2dc05ae60fc6d1b06baa34bc3
JungMin Ahn <jungminahn.minsone@gmail.com>
2020-08-31 01:08:36 +0900

    Package.swift 코드 정리

--- Package.swift
+++ Package.swift
@@ -3,37 +3,35 @@
 
 import PackageDescription
 
-let kakaoSDKSourcePath = "Sources/kakao-ios-sdk-2.0.1/sources"
-
 let package = Package(
     name: "KakaoSDK",
     platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "KakaoSDKCommon",
-            targets: ["KakaoSDKCommon"]),
+            name: KakaoSDK.common.name,
+            targets: [KakaoSDK.common.name]),
         .library(
-            name: "KakaoSDKAuth",
-            targets: ["KakaoSDKAuth"]),
+            name: KakaoSDK.auth.name,
+            targets: [KakaoSDK.auth.name]),
         .library(
-            name: "KakaoSDKTemplate",
-            targets: ["KakaoSDKTemplate"]),
+            name: KakaoSDK.template.name,
+            targets: [KakaoSDK.template.name]),
         .library(
-            name: "KakaoSDKLink",
-            targets: ["KakaoSDKLink"]),
+            name: KakaoSDK.link.name,
+            targets: [KakaoSDK.link.name]),
         .library(
-            name: "KakaoSDKUser",
-            targets: ["KakaoSDKUser"]),
+            name: KakaoSDK.user.name,
+            targets: [KakaoSDK.user.name]),
         .library(
-            name: "KakaoSDKTalk",
-            targets: ["KakaoSDKTalk"]),
+            name: KakaoSDK.talk.name,
+            targets: [KakaoSDK.talk.name]),
         .library(
-            name: "KakaoSDKStory",
-            targets: ["KakaoSDKStory"]),
+            name: KakaoSDK.story.name,
+            targets: [KakaoSDK.story.name]),
         .library(
-            name: "KakaoSDKNavi",
-            targets: ["KakaoSDKNavi"]),
+            name: KakaoSDK.navi.name,
+            targets: [KakaoSDK.navi.name]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -44,40 +42,78 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "DynamicCodable",
+            name: Dependency.dynamicCodable.name,
             dependencies: [],
-            path: "Package/DynamicCodable/Sources"),
+            path: Dependency.dynamicCodable.path),
         .target(
-            name: "KakaoSDKCommon",
-            dependencies: ["Alamofire", "DynamicCodable"],
-            path: "\(kakaoSDKSourcePath)/KakaoSDKCommon"),
+            name: KakaoSDK.common.name,
+            dependencies: [.alamofire, .dynamicCodable],
+            path: KakaoSDK.common.path),
         .target(
-            name: "KakaoSDKAuth",
-            dependencies: ["Alamofire", "DynamicCodable", "KakaoSDKCommon"],
-            path: "\(kakaoSDKSourcePath)/KakaoSDKAuth"),
+            name: KakaoSDK.auth.name,
+            dependencies: [.alamofire, .dynamicCodable, .kakaoSDKcommon],
+            path: KakaoSDK.auth.path),
         .target(
-            name: "KakaoSDKTemplate",
-            dependencies: ["KakaoSDKCommon"],
-            path: "\(kakaoSDKSourcePath)/KakaoSDKTemplate"),
+            name: KakaoSDK.template.name,
+            dependencies: [.kakaoSDKcommon],
+            path: KakaoSDK.template.path),
         .target(
-            name: "KakaoSDKLink",
-            dependencies: ["KakaoSDKCommon", "KakaoSDKTemplate"],
-            path: "\(kakaoSDKSourcePath)/KakaoSDKLink"),
+            name: KakaoSDK.link.name,
+            dependencies: [.kakaoSDKcommon, .kakaoSDKTemplate],
+            path: KakaoSDK.link.path),
         .target(
-            name: "KakaoSDKUser",
-            dependencies: ["KakaoSDKCommon", "KakaoSDKAuth"],
-            path: "\(kakaoSDKSourcePath)/KakaoSDKUser"),
+            name: KakaoSDK.user.name,
+            dependencies: [.kakaoSDKcommon, .kakaoSDKAuth],
+            path: KakaoSDK.user.path),
         .target(
-            name: "KakaoSDKTalk",
-            dependencies: ["KakaoSDKCommon", "KakaoSDKAuth", "KakaoSDKTemplate"],
-            path: "\(kakaoSDKSourcePath)/KakaoSDKTalk"),
+            name: KakaoSDK.talk.name,
+            dependencies: [.kakaoSDKcommon, .kakaoSDKAuth, .kakaoSDKTemplate],
+            path: KakaoSDK.talk.path),
         .target(
-            name: "KakaoSDKStory",
-            dependencies: ["KakaoSDKCommon", "KakaoSDKAuth"],
-            path: "\(kakaoSDKSourcePath)/KakaoSDKStory"),
+            name: KakaoSDK.story.name,
+            dependencies: [.kakaoSDKcommon, .kakaoSDKAuth],
+            path: KakaoSDK.story.path),
         .target(
-            name: "KakaoSDKNavi",
-            dependencies: ["KakaoSDKCommon", "KakaoSDKAuth"],
-            path: "\(kakaoSDKSourcePath)/KakaoSDKNavi"),
+            name: KakaoSDK.navi.name,
+            dependencies: [.kakaoSDKcommon, .kakaoSDKAuth],
+            path: KakaoSDK.navi.path),
     ]
 )
+
+
+// MARK: - Utility
+enum KakaoSDK: String {
+    case common = "KakaoSDKCommon"
+    case auth = "KakaoSDKAuth"
+    case template = "KakaoSDKTemplate"
+    case link = "KakaoSDKLink"
+    case user = "KakaoSDKUser"
+    case talk = "KakaoSDKTalk"
+    case story = "KakaoSDKStory"
+    case navi = "KakaoSDKNavi"
+    
+    var version: String { "2.0.1" }
+    var path: String { "Sources/kakao-ios-sdk-\(version)/sources/\(rawValue)" }
+    var name: String { rawValue }
+    var dependency: Target.Dependency { .init(stringLiteral: name) }
+}
+
+enum Dependency: String {
+    case alamofire = "Alamofire"
+    case dynamicCodable = "DynamicCodable"
+
+    var name: String { return rawValue }
+    var dependency: Target.Dependency { .init(stringLiteral: name) }
+    var path: String? {
+        if case .dynamicCodable = self { return "Package/DynamicCodable/Sources" }
+        return nil
+    }
+}
+
+extension Target.Dependency {
+    static var kakaoSDKcommon: Self { Target.Dependency(stringLiteral: KakaoSDK.common.name) }
+    static var kakaoSDKAuth: Self { Target.Dependency(stringLiteral: KakaoSDK.auth.name) }
+    static var kakaoSDKTemplate: Self { Target.Dependency(stringLiteral: KakaoSDK.template.name) }
+    static var alamofire: Self { Target.Dependency(stringLiteral: Dependency.alamofire.name) }
+    static var dynamicCodable: Self { Target.Dependency(stringLiteral: Dependency.dynamicCodable.name) }
+}
